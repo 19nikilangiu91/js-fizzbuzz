5286401d5b3f648f4b32966a688b892f4dbdc3db
19nikilangiu91 <niki.langiu@gmail.com>
2022-11-09 20:18:41 +0100

    Creazione Container-div-square

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
 // Creo un ciclo per la nostra variabile "i" da "1" a "100".
 for( let i = 1; i <= 100; i++){
     console.log(i)
+
+    let contenitore = document.querySelector(".container");
+
+    // Creo dei "div"
+    let element = document.createElement("div");
+
+    // Aggiungo un'altra classe ai "div".
+    element.classList.add("square");
+
+    // Al contenitore andiamo ad aggiungere "element".
+    contenitore.append(element);
+
+
 }
